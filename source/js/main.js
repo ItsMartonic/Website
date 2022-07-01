97c6920ac0576298059f9b997c6441be4a15de9d
ItsMartonic <OfficialMartonic@gmail.com>
2022-07-01 11:57:20 +1000

    Reset

--- source/js/main.js
+++
@@ -1,9 +0,0 @@
-// Navigation
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
